f1752c0f696e87e74c579552e5dbc4c7a079c677
vaibhavprakash55 <vaibhaveng23.mes27@gmail.com>
2026-02-04 00:17:31 +0530

    add problem

---
+++ reversebubble.java
@@ -0,0 +1,27 @@
+// arrange in descending order using bubble sort
+import java.util.Scanner;
+public class reversebubble {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter size of array: ");
+    int n = in.nextInt();
+    int[] arr = new int[n];
+    System.out.println("Enter elements: ");
+    for(int i=0;i<n;i++){
+      arr[i] = in.nextInt();
+    }
+    for(int i=0;i<n-1;i++){
+      for(int j=0;j<n-i-1;j++){
+        if(arr[j]<arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
+      }
+    }
+    for(int i=0;i<n;i++){
+      System.out.print(arr[i]+" ");
+    }
+  }
+  
+}
