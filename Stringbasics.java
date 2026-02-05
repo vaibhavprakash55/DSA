4bd05dca357f62c097d431e91d846727eb2c3c80
vaibhavprakash55 <vaibhaveng23.mes27@gmail.com>
2026-02-05 23:14:23 +0530

    add string basics

---
+++ Stringbasics.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Stringbasics {
+  public static void main(String[] args) {
+    char [] arr={'v','a','i','b','h','a','v'};
+    for(char ele:arr){
+      System.out.print (ele+" ");
+    }
+    
+  }
+  
+}
