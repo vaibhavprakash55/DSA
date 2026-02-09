ee616143864ed7ea5786e86268358190685ecbd6
vaibhavprakash55 <vaibhaveng23.mes27@gmail.com>
2026-02-09 23:47:47 +0530

    add 2D array problem

---
+++ OutputIn2D.java
@@ -0,0 +1,12 @@
+public class OutputIn2D {
+  public static void main(String[] args) {
+    int [][] arr = new int[3][4];
+    for(int i=0;i<3;i++){
+      for(int j=0;j<4;j++){
+        System.out.print(arr[i][j]+" ");
+      }
+      System.out.println();
+    }
+  }
+  
+}
